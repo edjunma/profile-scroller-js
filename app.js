2204d92c55b244a6910a27e935dd321d9c789aad
edjunma <junwenma@buffalo.edu>
2019-03-16 00:30:42 -0400

    Next iteration added

--- app.js
+++ app.js
@@ -28,6 +28,9 @@ const data = [
 
 const profiles = profileIterator(data);
 
+// Call first profile
+nextProfile();
+
 // Click Event
 document.getElementById("next").addEventListener("click", nextProfile);
 
@@ -35,7 +38,8 @@ document.getElementById("next").addEventListener("click", nextProfile);
 function nextProfile() {
   const currentProfile = profiles.next().value;
 
-  document.getElementById("profileDisplay").innerHTML = `
+  if (currentProfile !== undefined) {
+    document.getElementById("profileDisplay").innerHTML = `
     <ul class="list-group">
       <li class="list-group-item">Name: ${currentProfile.name}</li>
       <li class="list-group-item">Age: ${currentProfile.age}</li>
@@ -46,9 +50,13 @@ function nextProfile() {
     </ul>
   `;
 
-  document.getElementById('imageDisplay').innerHTML = `<img 
-  src="${}">`;
-}currentProfile.image
+    document.getElementById("imageDisplay").innerHTML = `<img 
+  src="${currentProfile.image}">`;
+  } else {
+    // No more profiles
+    window.location.reload();
+  }
+}
 
 // Profile Iterator
 function profileIterator(profiles) {
