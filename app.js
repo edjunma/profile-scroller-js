58c7cb46336d025e7db57a1b318d0f8600b37075
edjunma <junwenma@buffalo.edu>
2019-03-16 00:15:14 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+const data = [
+  // Can fetch API for info but will be hardcode for filler
+  {
+    name: "John Doe",
+    age: 23,
+    gender: "Male",
+    lookingfor: "Female",
+    location: "Buffalo NY",
+    image: "https://randomuser.me/api/portraits/men/82.jpg"
+  },
+  {
+    name: "Jen Smith",
+    age: 26,
+    gender: "Female",
+    lookingfor: "Male",
+    location: "Miama FL",
+    image: "https://randomuser.me/api/portraits/women/82.jpg"
+  },
+  {
+    name: "Emilia Clarke",
+    age: 28,
+    gender: "Female",
+    lookingfor: "Jon Snow",
+    location: "Westeros",
+    image: "https://randomuser.me/api/portraits/women/83.jpg"
+  }
+];
