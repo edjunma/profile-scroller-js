fe12e0a364723b5dbdf9c41c4626399068a82a25
edjunma <junwenma@buffalo.edu>
2019-03-16 00:17:33 -0400

    Added profile iterator

--- app.js
+++ app.js
@@ -25,3 +25,16 @@ const data = [
     image: "https://randomuser.me/api/portraits/women/83.jpg"
   }
 ];
+
+// Profile Iterator
+function profileIterator(profiles) {
+  let nextIndex = 0;
+
+  return {
+    next: function() {
+      return nextIndex < profiles.length
+        ? { value: profiles[nextIndex++], done: false }
+        : { done: true };
+    }
+  };
+}
